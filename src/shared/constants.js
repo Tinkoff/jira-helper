d41660dd6bc29982807acd243f8833b6ff65ce5f
Akhmetchanov Pavel Aleksandrovich <p.akhmetchanov@tinkoff.ru>
2020-03-21 22:47:46 +0300

    create image branch

--- src/shared/constants.js
+++
@@ -1,7 +0,0 @@
-export const BOARD_PROPERTIES = {
-  WIP_LIMITS_SETTINGS: 'subgroupsJH',
-  SWIMLANE_SETTINGS: 'jiraHelperSwimlaneSettings',
-  OLD_SWIMLANE_SETTINGS: 'jiraHelperWIPLimits',
-  SLA_CONFIG: 'slaConfig3',
-  TETRIS_PLANNING: 'settingTetrisPlaning',
-};
